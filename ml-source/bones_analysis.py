cd259239526ffe1190f4e861c1e76fa458078b41
rshrc <rishieric91@gmail.com>
2018-08-15 21:33:54 +0530

    Rows Dropped and Columns Renamed

--- ml-source/bones_analysis.py
+++ ml-source/bones_analysis.py
@@ -4,3 +4,14 @@ import matplotlib.pyplot as plt
 
 dataset = pd.read_excel('data/doctors.xlsx')
 
+# Dropping the first row which contains all NaN data
+dataset = dataset.drop(dataset.index[[0,2]])
+
+dataset = dataset.rename(columns=
+                         {'Type': 'Row Labels', 
+                          'TRUE': 'Count of Doctors',
+                          'Unnamed: 2': 'Centers',
+                          'Unnamed: 3': 'Population',
+                          'Unnamed: 4': 'Areas',
+                          'Unnamed: 5': 'Zones',
+                          })
